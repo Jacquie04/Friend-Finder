85d718d429c7d2c06e286143dcce2628315ab56e
Jacquelyn Duley <45111833+Jacquie04@users.noreply.github.com>
2019-03-19 11:57:13 -0700

    friend update

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendArray = [
+{
+    name:"Rick Sanchez",
+    photo:"https://vignette.wikia.nocookie.net/rickandmorty/images/a/a6/Rick_Sanchez.png/revision/latest?cb=20160923150728",
+    scores:[
+        1,
+        5,
+        1,
+        5,
+        1,
+        5,
+        3,
+        5,
+        1,
+        1
+      ]
+  }
+];
+
+module.exports = friendArray;
