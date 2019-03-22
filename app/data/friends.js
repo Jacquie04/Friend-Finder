065cd210fd17d14ce1b6cf8c5c30cdaa1a602b30
Jacquelyn Duley <45111833+Jacquie04@users.noreply.github.com>
2019-03-21 18:15:34 -0700

    character

--- app/data/friends.js
+++ app/data/friends.js
@@ -15,6 +15,38 @@ var friendArray = [
         1
       ]
   }
+  {
+    name:"Morty",
+    photo:"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjK1NyRyZThAhUUuJ4KHfEGD-oQjRx6BAgBEAU&url=https%3A%2F%2Fgeeks.media%2Frick-and-morty-fan-theories&psig=AOvVaw16rcptWTY9HCPn5OjqJ6fA&ust=1553303645339496",
+    scores:[
+        5,
+        1,
+        5,
+        1,
+        5,
+        1,
+        3,
+        1,
+        5,
+        5
+      ]
+  }
+  {
+    name:"Beth",
+    photo:"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwih2_-fyZThAhVKtZ4KHcmpB5AQjRx6BAgBEAU&url=https%3A%2F%2Frickandmorty.fandom.com%2Fwiki%2FBeth_Smith&psig=AOvVaw1bmZA6yU7Ep7D8fBWdOhHw&ust=1553303673391122",
+    scores:[
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3
+      ]
+  }
 ];
 
 
